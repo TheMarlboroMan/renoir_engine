736db8a3ea474620193af95f36f0a8c5041e91e7
TheMarlboroMan <marlborometal@gmail.com>
2018-02-21 12:20:26 +0100

    Nested foreach is ok. New put is ok. Nested ifs are not ok

--- examples/test-view.php
+++ examples/test-view.php
@@ -13,34 +13,70 @@ set_error_handler(function ($err_severity, $err_msg, $err_file, $err_line, array
 use Renoir_engine\View\View;
 
 try{
-	$test=<<<R
-<h1>Hello!! {{   put  [myvar]}} </h1>
-<p>This is something</p>
-{{ foreach myarray as value }}
-<p>We do stuff to {{put [value]}}</p>
-{{ endforeach }}
-<p>And we are done!!</p>
-{{if myvar != null then}}
-<p>Myvar is not null</p>
-{{endif
+/*
+<h1>{{put[header]}}</h1>
+<p>I have a few things:</p>
+<ul>
+{{foreach things as item}}
+	<li>
+		{{put [item>val, " has a few things of its own:"]}}
+		<ul>
+			{{foreach item*get_data as inner}}
+				<li>{{put [inner]}}</li>
+			{{endforeach}}
+		</ul>
+	</li>
+{{endforeach}}
+<ul>
+<p>I also have some words!!!</p>
+{{foreach words as word}}
+<p>
+	<b>{{put [word]}}</b>
+</p>
+{{endforeach}}
+<p>Finally I also have a number, {{put [number]}} which:
+*/
 
-if myvar == "World!" then}}
-<p>My var is world, actually</p>
-{{else}}
-<p>My var is not world</p>
-{{endif}}
-<p>Finally {{put [myarray.2]}} and {{put [thing.key>val]}}</p>
+	$template_str=<<<R
+{{if number > 10 then 
+	put ["is larger than 10"]
+	if number > 100 then
+		put [" and is also larger than 100"]
+		if number > 1000 then
+			put [" and is larger than 1000"]
+		else
+			put [" but is not larger than 1000"]
+		endif
+	else
+		put [" but is not larger than 100"]
+	endif
+else
+	put ["is not larger than 10 "]
+endif
+}}
 R;
-
-$test='HELLO {{put[myvar,10 , "const[]{{}}lolol" ]}} kTHNXBYE!';
 
 	class Thing {
 		public $val;
-		public function __construct($v) {$this->val=$v;}
+		private $data=[];
+		public function __construct($v, array $d) {
+			$this->val=$v;
+			$this->data=$d;
+		}
+		public function get_data() {
+			return $this->data;
+		}
 	}
 
+	$things=[new Thing("a value", [1, 2, 3]), new Thing("another value",[])];
+
 	$v=new View();
-	echo $v->set_template_string($test)->set('thing', ['key' => new Thing('cosa')])->set('myvar', 'World!')->set('myarray', ['each', 'and', 'everyone'])->render();
+	echo $v->set_template_string($template_str)
+		->set('header', 'My header!')
+		->set('things', $things)
+		->set('words', ['each', 'and', 'every', 'word'])
+		->set('number', 199)
+		->render();
 }
 catch(Exception $e) {
 	echo 'Error: '.$e->getMessage();
